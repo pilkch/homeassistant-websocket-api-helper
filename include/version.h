099b97d15162a5a5ef52373ac2f8d5dc4ada3726
Chris Pilkington <chris.pilkington@gmail.com>
2024-01-30 23:30:41 +1100

    Added command line arguments.

---
+++ include/version.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace application {
+
+const std::string version = "0.9";
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,58 @@
 #include <iostream>
 
+#include <sysexits.h>
+
 #include "curl_helper.h"
 #include "home_assistant.h"
 #include "settings.h"
+#include "version.h"
 #include "web_socket.h"
 
-int main()
+namespace application {
+
+void PrintUsage()
 {
+  std::cout<<"Usage: ./homeassistant-websocket-api-helper [OPTION]"<<std::endl;
+  std::cout<<std::endl;
+  std::cout<<"  -v, --version                 Print the version and exit"<<std::endl;
+  std::cout<<"  --create-and-download-backup  Create a backup and download it to a tar file in the current directory"<<std::endl;
+}
+
+void PrintVersion()
+{
+  std::cout<<"homeassistant-websocket-api-helper version "<<version<<std::endl;
+}
+
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2) {
+    // Incorrect number of arguments, print the usage and exit
+    application::PrintUsage();
+    return EX_USAGE;
+  }
+
+  const std::string argument(argv[1]);
+  if ((argument == "-v") || (argument == "--version")) {
+    application::PrintVersion();
+    return EXIT_SUCCESS;
+  } else if ((argument != "--create-and-download-backup")) {
+    // Unknown argument, print the usage and exit
+    application::PrintUsage();
+    return EX_USAGE;
+  }
+
+  // Must be "--create-and-download-backup"
+
+  // Parse the configuration file
   application::cSettings settings;
   if (!settings.LoadFromFile("./configuration.json")) {
     std::cerr<<"Error parsing configuration.json"<<std::endl;
     return EXIT_FAILURE;
   }
 
+  // Create a backup and download it
   {
     curl::cCurlHelper helper;
 
